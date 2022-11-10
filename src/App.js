c8d3b28b74bbc6a8b3c0e006a7dbe9b7aae5177f
acimack <70112919+acimack@users.noreply.github.com>
2022-11-10 17:12:17 -0500

    rough

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import { BakeryItem } from "./components/BakeryItem";
+import { Button } from '@mui/material';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from 'bootstrap';
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,19 +15,54 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [currCart, setCurrCart] = useState({});
+  const [price, setPrice] = useState(0);
+
+  function addToCart(item) {
+    // setCurrCart(currCart.concat(item));
+    
+    if (currCart[item.name] == null) {
+      currCart[item.name] = 1;
+    }
+    else {
+      currCart[item.name] = currCart[item.name] + 1;
+    }
+    setPrice(price + item.price)
+    console.log(currCart);
+  }
 
   return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+    <div className="App flex flex-col items-center">
+      <div className="flex w-full sm:max-w-[1280px]"> 
+      <div className="flex flex-col w-2/3 p-8 gap-8">
+        <div className="flex flex-col w-2/3 p-8 gap-8">
+          <h1>My Bakery</h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {bakeryData.map((item, index) => (
+            <BakeryItem 
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+              add={addToCart}
+              item={item}
+            ></BakeryItem>
+          ))}
+        </div>
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
       </div>
+    </div>
+    <div className="Cart">
+      <h2>Cart</h2>
+      {
+        Object.keys(currCart).map(function(key, index) {
+          return <p>{currCart[key]}x {key}</p>;
+        })
+      }
+      <h3>Price: {price}</h3>
+      <Button>Checkout</Button>
+    </div>
     </div>
   );
 }
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,32 @@
 // TODO: create a component that displays a single bakery item
+
+// import { getImageUrl } from './utils.js';
+import React from "react";
+import { Button } from '@mui/material';
+
+export function BakeryItem({ name, description, price, image, item, add }) {
+  return (
+    <div className="BakeryItem bg-gray-50 dark:bg-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-md flex flex-col">
+     <div>
+      <img
+            // className="avatar"
+            src={image}
+            alt={name}
+            width={"100px"}
+            height={"100px"}
+          />
+      </div>
+      
+      <div className="item-des">
+        <h4>{name}</h4>
+          <h6>{description}</h6>
+          <div className="addToCart flex items-center justify-between">
+          <h5>{price}</h5>
+            <Button onClick={()=>{add(item)}}>Add to Cart</Button>
+          </div>
+      </div>
+          
+    </div>
+  );
+}
+
